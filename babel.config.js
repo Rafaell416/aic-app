98c32dc285669fc1ade5ba0ddef909a9db6d40c6
Rafael Villarreal <rvillarreal416@gmail.com>
2023-12-30 16:19:59 -0500

    feat: add babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,48 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    env: {
+      production: {
+        plugins: ["transform-remove-console"],
+      },
+    },
+    plugins: [
+      "react-native-reanimated/plugin",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@components": "./src/components",
+            "@theme": "./src/theme",
+            "@hooks": "./src/hooks",
+            "@utils": "./src/utils",
+            "@api": "./src/api"
+          },
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".ios.js",
+            ".android.js",
+            ".json",
+            ".png",
+            ".jpg",
+            ".webp",
+            ".gif",
+            ".svg",
+            ".ttf",
+            ".mp3",
+            ".mp4",
+          ],
+        },
+      ],
+      ["transform-remove-console", { exclude: ["log"] }],
+    ]
+  }
+}
