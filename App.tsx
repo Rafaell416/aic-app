51e8a4fa3ff6bc49cb62e9338e25e801fa47d665
Rafael Villarreal <rvillarreal416@gmail.com>
2023-12-30 16:16:18 -0500

    feat: add assets and fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { useCallback } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'PlayfairDisplay-Bold': require('./assets/fonts/PlayfairDisplay-Bold.ttf'),
+    'PlayfairDisplay-Medium': require('./assets/fonts/PlayfairDisplay-Medium.ttf'),
+    'PlayfairDisplay-Regular': require('./assets/fonts/PlayfairDisplay-Regular.ttf'),
+    'PlayfairDisplay-SemiBold': require('./assets/fonts/PlayfairDisplay-SemiBold.ttf'),
+    'ProzaLibre-Bold': require('./assets/fonts/ProzaLibre-Bold.ttf'),
+    'ProzaLibre-Light': require('./assets/fonts/ProzaLibre-Light.ttf'),
+    'ProzaLibre-Medium': require('./assets/fonts/ProzaLibre-Medium.ttf'),
+    'ProzaLibre-Regular': require('./assets/fonts/ProzaLibre-Regular.ttf'),
+    'ProzaLibre-SemiBold': require('./assets/fonts/ProzaLibre-SemiBold.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{fontFamily: 'PlayfairDisplay-Medium'}}>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontFamily: 'ProzaLibre-Regular'}}>Open up App.tsx to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
