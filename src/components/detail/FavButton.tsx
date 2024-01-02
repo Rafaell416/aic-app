e2dc86a6c41c3bdf08343fdae8f5b786a7c0ce0f
Rafael Villarreal <rvillarreal416@gmail.com>
2024-01-02 15:11:18 -0500

    feat: add fav button animation

--- src/components/detail/FavButton.tsx
+++ src/components/detail/FavButton.tsx
@@ -1,9 +1,14 @@
-import Box from "@components/common/Box";
+import { AnimatedBox } from "@components/common/Box";
 import { AntDesign as Icon } from "@expo/vector-icons";
 import colors from "@theme/colors";
 import shadows from "@theme/shadows";
 import { memo, useState } from "react";
 import { Pressable } from "react-native";
+import {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
 
 interface FavButtonProps {
   onPress: () => void;
@@ -12,18 +17,31 @@ interface FavButtonProps {
 
 const FavButton: React.FC<FavButtonProps> = ({ onPress, eventSaved }) => {
   const [checked, setChecked] = useState(eventSaved);
+  const scale = useSharedValue(1);
 
   const handlePress = () => {
     onPress?.();
     setChecked(!checked);
+
+    scale.value = withSpring(checked ? 1 : 1.3, { damping: 2 }, () => {
+      if (checked) {
+        scale.value = withSpring(1);
+      }
+    });
   };
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
 
   const iconName = checked ? "heart" : "hearto";
   const iconColor = checked ? colors.red : colors.black;
 
   return (
     <Pressable onPress={handlePress}>
-      <Box
+      <AnimatedBox
         backgroundColor="white"
         height={35}
         width={35}
@@ -32,9 +50,10 @@ const FavButton: React.FC<FavButtonProps> = ({ onPress, eventSaved }) => {
         justifyContent="center"
         elevation={5}
         {...shadows.ios}
+        style={animatedStyles}
       >
         <Icon name={iconName} size={20} color={iconColor} />
-      </Box>
+      </AnimatedBox>
     </Pressable>
   );
 };
